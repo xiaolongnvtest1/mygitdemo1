4fd24ddc71a25c5716b940328c6b0a00de2d4318
xiaolongnv1 <xiaolongnv@163.com>
2021-09-22 22:21:36 +0800

    idea 第2次提交项目到git

--- src/main/java/com/lina/git/MyTest.java
+++ src/main/java/com/lina/git/MyTest.java
@@ -1,10 +1,11 @@
 package com.lina.git;
 
 /**
- * @Author wanglina
+ * @Author lina
  */
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git222!!!");
     }
 }
