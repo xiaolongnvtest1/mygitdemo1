daa3e760b66614b178214a2c50c0b3c29e4a1c0d
xiaolongnv1 <xiaolongnv@163.com>
2021-09-22 22:22:41 +0800

    idea 第3次提交项目到git

--- src/main/java/com/lina/git/MyTest.java
+++ src/main/java/com/lina/git/MyTest.java
@@ -7,5 +7,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("hello git222!!!");
+        System.out.println("hello git333!!!");
     }
 }
